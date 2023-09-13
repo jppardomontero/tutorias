24b94e1102097161cdeaeb697a11b0f8a1b185f3
jppardomontero <jppardo@marianosamaniego.edu.ec>
2023-09-13 08:25:11 -0500

    actualización

--- proveedor.py
+++ proveedor.py
@@ -1,4 +1,14 @@
 from tkinter import *
+from conexion import conexion
+from tkinter import messagebox
+
+def guardarProveedor():
+    sql = "INSERT INTO proveedor (cedula, nombres, empresa, correo, direccion) VALUES(?,?,?,?,?)" 
+    parametros = (txtCedula.get(), txtNombres.get(), txtEmpresa.get(), txtCorreo.get(), txtDireccion.get())
+    if conexion.ejecutar(sql, parametros):
+        messagebox.showinfo(message="Los datos del proveedor guardaos con éxito", title="Guardar datos")
+    else:
+        messagebox.showwarning(message="Los dotos no pueden ser guardados", title="Guardos datos")
 
 ventanaProveedor = Tk()
 ventanaProveedor.title("Adminitración de proveedores")
@@ -27,6 +37,6 @@ Label(framePrincipal, text="Direccion:").grid(column=2, row=2, padx=3, pady=3)
 txtDireccion = Entry(framePrincipal)
 txtDireccion.grid(column=3, row=2, padx=3, pady=3)
 
-btnGuardar = Button(ventanaProveedor, text="Guardar")
+btnGuardar = Button(ventanaProveedor, text="Guardar",command=guardarProveedor)
 btnGuardar.grid(column=0,row=3, padx=3,pady=3, columnspan=2)
 ventanaProveedor.mainloop()
